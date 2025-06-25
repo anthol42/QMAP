81c4257637e07802560b3546f0144a51fed093bb
Anthony Lavertu <alavertu2@gmail.com>
2025-06-25 17:25:13 -0400

    Feat(new_better_config)

--- split_experiment/mmseqs/cluster_mmseqs.sh
+++ split_experiment/mmseqs/cluster_mmseqs.sh
@@ -9,9 +9,9 @@ mmseqs createdb "$INPUT" "$DB"
 # Cluster with short-peptide optimized settings
 mmseqs linclust "$DB" "$CLUSTERS" "$TMP" \
     --min-seq-id 0.5 \
-    --cov-mode 5 -c 0.5 \
+    --cov-mode 0 -c 0. \
     --cluster-mode 1 \
-    --kmer-per-seq 1000 \
+    --kmer-per-seq 100000 \
     --comp-bias-corr 0
 
 # Export representative sequences of clusters
