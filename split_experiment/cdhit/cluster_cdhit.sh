a590098dcff50883adc21bf7267e2e8cca77b0bb
Anthony Lavertu <alavertu2@gmail.com>
2025-06-25 19:54:02 -0400

    Feat(CD-HIt better config)

--- split_experiment/cdhit/cluster_cdhit.sh
+++ split_experiment/cdhit/cluster_cdhit.sh
@@ -1,4 +1,4 @@
 INPUT="../.cache/cdhit/dataset.fasta"
 CLUSTERS="../.cache/cdhit/clusters"
 
-cd-hit -i "$INPUT" -o "$CLUSTERS" -c 0.3 -n 2 -d 0 -M 30000 -T 10
+cd-hit -i "$INPUT" -o "$CLUSTERS" -c 0.5 -n 2 -d 0 -M 30000 -T 10
