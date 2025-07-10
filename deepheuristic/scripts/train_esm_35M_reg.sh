1a32070fe73aff1631cf2b830d4c1ce1bd0973a2
Anthony Lavertu <alavertu2@gmail.com>
2025-07-10 14:48:03 -0400

    Fix(reg script): Increase the time limit

--- deepheuristic/scripts/train_esm_35M_reg.sh
+++ deepheuristic/scripts/train_esm_35M_reg.sh
@@ -2,7 +2,7 @@
 #SBATCH --array=0-3
 #SBATCH --output=logs/%x_%A_%a.log
 #SBATCH --error=logs/%x_%A_%a.log
-#SBATCH --time=2:45:00
+#SBATCH --time=3:30:00
 #SBATCH --mem=16Gb
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
