98fc30165d76d09d5940650e52e1b7738a484f11
Anthony Lavertu <alavertu2@gmail.com>
2025-07-16 09:38:58 -0400

    Feat(hsearch 35M)

--- deepheuristic/scripts/train_esm_35M_hsearch.sh
+++ deepheuristic/scripts/train_esm_35M_hsearch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --array=0-2
+#SBATCH --array=0-3
 #SBATCH --output=logs/%x_%A_%a.log
 #SBATCH --error=logs/%x_%A_%a.log
-#SBATCH --time=70:00:00
-#SBATCH --mem=16Gb
+#SBATCH --time=72:00:00
+#SBATCH --mem=20Gb
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 
@@ -16,7 +16,7 @@ echo "Running on node: $HOSTNAME"
 echo "Start time: $(date)"
 
 # Run the command
-uv run main.py --experiment=experiment1_hsearch --config=configs/ESM_35M.yml --fract=0.1 --verbose=1 --n_trials=18
+uv run main.py --experiment=experiment1_hsearch --config=configs/ESM_35M.yml --fract=0.05 --verbose=1 --n_trials=10
 
 echo "End time: $(date)"
 echo "Job completed"
