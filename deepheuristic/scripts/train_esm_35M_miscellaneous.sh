f6fe1b951df72d55415e31c1302d2d0932842a49
Anthony Lavertu <alavertu2@gmail.com>
2025-07-19 08:43:34 -0400

    Feat(final training script)

--- deepheuristic/scripts/train_esm_35M_miscellaneous.sh
+++ deepheuristic/scripts/train_esm_35M_miscellaneous.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --output=logs/%x_%A_%a.log
 #SBATCH --error=logs/%x_%A_%a.log
-#SBATCH --time=7:00:00
-#SBATCH --mem=16Gb
+#SBATCH --time=95:00:00
+#SBATCH --mem=32Gb
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 
@@ -15,7 +15,7 @@ echo "Running on node: $HOSTNAME"
 echo "Start time: $(date)"
 
 # Run the command
-uv run main.py --experiment=experiment1 --config=configs/ESM_35M.yml --fract=0.05 --verbose=2
+uv run main.py --experiment=experiment1 --config=configs/ESM_35M.yml --verbose=2
 
 echo "End time: $(date)"
 echo "Job completed"
